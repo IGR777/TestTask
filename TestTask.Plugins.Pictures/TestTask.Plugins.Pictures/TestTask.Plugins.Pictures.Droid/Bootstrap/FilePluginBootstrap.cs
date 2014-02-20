51215585aa7e357a9e60c7e6a8bbea48d54855cf
igr777 <igr-777@yandex.ru>
2014-02-20 23:34:01 +0200

    Changed libraries references

---
+++ TestTask.Plugins.Pictures/TestTask.Plugins.Pictures/TestTask.Plugins.Pictures.Droid/Bootstrap/FilePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace TestTask.Plugins.Pictures.Droid.Bootstrap
+{
+    public class FilePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.File.PluginLoader>
+    {
+    }
+}
